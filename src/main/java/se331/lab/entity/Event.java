cb7e212ceac89544dee46b7e096c89577f2371f3
Eakkasit Nimanussornkul <eakkasit_nim@cmu.ac.th>
2025-09-04 22:10:29 +0700

    2.4 added event class with it's attribute

---
+++ src/main/java/se331/lab/entity/Event.java
@@ -0,0 +1,18 @@
+package se331.lab.entity;
+
+import lombok.Data;
+import lombok.Builder;
+
+@Data
+@Builder
+
+public class Event {
+    Long id;
+    String category;
+    String title;
+    String description;
+    String date;
+    String time;
+    Boolean petAllowed;
+    String organizer;
+}
