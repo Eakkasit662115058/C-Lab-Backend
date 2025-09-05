df7e21d57246e670dee3bde6b33b0082a07933ba
Eakkasit Nimanussornkul <eakkasit_nim@cmu.ac.th>
2025-09-05 09:42:10 +0700

    3.1 apidog

--- src/main/java/se331/lab/rest/controller/EventController.java
+++ src/main/java/se331/lab/rest/controller/EventController.java
@@ -3,7 +3,8 @@ package se331.lab.rest.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import se331.lab.entity.Event;
+import org.springframework.web.bind.annotation.RequestParam;
+import se331.lab.rest.entity.Event;
 
 import jakarta.annotation.PostConstruct;
 import java.util.List;
@@ -90,7 +91,15 @@ public class EventController {
     }
 
     @GetMapping("events")
-    public ResponseEntity<?> getEventLists(){
-        return ResponseEntity.ok(eventList);
+    public ResponseEntity<?> getEventLists(@RequestParam(value = "_limit", required = false) Integer perPage
+    ,@RequestParam(value = "_page",required = false) Integer page) {
+        perPage = perPage == null ?eventList.size() : perPage;
+        page = page == null ? 1 : page;
+        Integer firstIndex = (page-1)*perPage;
+        List<Event> output = new ArrayList<>();
+        for (int i  = firstIndex; i < firstIndex + perPage; i++) {
+            output.add(eventList.get(i));
+        }
+        return ResponseEntity.ok(output);
     }
 }
--- src/main/java/se331/lab/rest/entity/Event.java
+++ src/main/java/se331/lab/rest/entity/Event.java
@@ -1,4 +1,4 @@
-package se331.lab.entity;
+package se331.lab.rest.entity;
 
 import lombok.Data;
 import lombok.Builder;
