6d036941823c123570356d0090ffcd1564796b08
Eakkasit Nimanussornkul <eakkasit_nim@cmu.ac.th>
2025-09-12 01:06:43 +0700

    add the controller to fetch the organizer using the pagination, and the query by id , the service, and dao

--- src/main/java/se331/lab/controller/EventController.java
+++ src/main/java/se331/lab/controller/EventController.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.server.ResponseStatusException;
 import se331.lab.entity.Event;
+import se331.lab.entity.Organizer;
 import se331.lab.service.EventService;
+import se331.lab.service.OrganizerService;
 
 import java.util.List;
 
@@ -20,6 +22,7 @@ import java.util.List;
 public class EventController {
 
     final EventService eventService;
+    final OrganizerService organizerService;
 
     @GetMapping("events")
     public ResponseEntity<?> getEventLists(@RequestParam(value = "_limit", required = false) Integer perPage
@@ -39,6 +42,31 @@ public class EventController {
     @GetMapping("events/{id}")
     public ResponseEntity<?> getEvent(@PathVariable ("id") long id) {
         Event output = eventService.getEvent(id);
+        if (output != null) {
+            return ResponseEntity.ok(output);
+        }else{
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"The given id is not found");
+        }
+    }
+
+    @GetMapping("organizers")
+    public ResponseEntity<?> getOrganizerLists(@RequestParam(value = "_limit", required = false) Integer perPage
+            ,@RequestParam(value = "_page",required = false) Integer page) {
+        List<Organizer> output = null;
+        Integer eventSize = organizerService.getOrganizerSize();
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.set("x-total-count", String.valueOf(eventSize));
+        try {
+            output = organizerService.getOrganizers(perPage, page);
+            return new ResponseEntity<>(output, responseHeaders, HttpStatus.OK);
+        }catch (IndexOutOfBoundsException ex) {
+            return new ResponseEntity<>(output, responseHeaders, HttpStatus.OK);
+        }
+    }
+
+    @GetMapping("organizers/{id}")
+    public ResponseEntity<?> getOrganizer(@PathVariable ("id") long id) {
+        Organizer output = organizerService.getOrganizer(id);
         if (output != null) {
             return ResponseEntity.ok(output);
         }else{
---
+++ src/main/java/se331/lab/dao/OrganizerDaolmpl.java
@@ -0,0 +1,82 @@
+package se331.lab.dao;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Repository;
+import se331.lab.entity.Organizer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class OrganizerDaolmpl implements OrganizerDao {
+    List<Organizer> organizerList;
+
+    @PostConstruct
+    public void init() {
+        organizerList = new ArrayList<>();
+        organizerList.add(Organizer.builder()
+                .id(123L)
+                .address("Meow Town")
+                .organizationName("Kat Laydee")
+                .build());
+
+        organizerList.add(Organizer.builder()
+                .id(456L)
+                .address("Flora City")
+                .organizationName("Fern Pollin")
+                .build());
+
+        organizerList.add(Organizer.builder()
+                .id(4582797L)
+                .address("Flora City")
+                .organizationName("Fern Pollin")
+                .build());
+
+        organizerList.add(Organizer.builder()
+                .id(8419988L)
+                .address("Playa Del Carmen")
+                .organizationName("Carey Wales")
+                .build());
+
+        organizerList.add(Organizer.builder()
+                .id(9238745L)
+                .address("Green Park Central")
+                .organizationName("EcoFuture Group")
+                .build());
+
+        organizerList.add(Organizer.builder()
+                .id(7293156L)
+                .address("Nature Reserve Trail")
+                .organizationName("Camera Club Collective")
+                .build());
+    }
+
+    @Override
+    public Integer getOrganizerSize(){
+        return organizerList.size();
+    }
+
+    @Override
+    public List<Organizer> getOrganizers(Integer pageSize, Integer page){
+        pageSize = pageSize == null? organizerList.size() : pageSize;
+        page = page == null ? 1: page;
+        Integer firstIndex = (page-1)*pageSize;
+        List<Organizer> output = new ArrayList<>();
+        for(int i = firstIndex; i < firstIndex+pageSize; i++){
+            output.add(organizerList.get(i));
+        }
+        return output;
+    }
+
+    @Override
+    public Organizer getOrganizer(Long id){
+        Organizer output = null;
+        for(Organizer organizer : organizerList){
+            if(organizer.getId().equals(id)){
+                output = organizer;
+                break;
+            }
+        }
+        return output;
+    }
+}
